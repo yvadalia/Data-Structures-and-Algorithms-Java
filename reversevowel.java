ab16a7490d37be595a0c9ffbffb2702a12279d4b
Yagnesh Vadalia <yagnesh92@gmail.com>
2017-02-20 22:15:49 -0500

    Update reversevowel.java

--- reversevowel.java
+++ reversevowel.java
@@ -1,17 +1,27 @@
 public class Solution {
     public String reverseVowels(String s) {
         if(s == null || s.length()==0) return s;
-        String vowels = "aeiouAEIOU";
+        HashSet<Character> vowel = new HashSet<Character>();
+        vowel.add('a');
+        vowel.add('e');
+        vowel.add('i');
+        vowel.add('o');
+        vowel.add('u');
+        vowel.add('A');
+        vowel.add('E');
+        vowel.add('I');
+        vowel.add('O');
+        vowel.add('U');
         char[] chars = s.toCharArray();
         int start = 0;
         int end = s.length()-1;
         while(start<end){
         
-            while(start<end && !vowels.contains(chars[start]+"")){
+            while(start<end && !vowel.contains(chars[start])){
                 start++;
             }
         
-            while(start<end && !vowels.contains(chars[end]+"")){
+            while(start<end && !vowel.contains(chars[end])){
                 end--;
             }
         
