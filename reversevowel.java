68a61f02368b18214820cd41896ce842a6423bc5
Yagnesh Vadalia <yagnesh92@gmail.com>
2017-02-20 22:11:38 -0500

    Update reversevowel.java

--- reversevowel.java
+++ reversevowel.java
@@ -1,31 +1,27 @@
 public class Solution {
     public String reverseVowels(String s) {
-        int[] pos = new int[s.length()];
-        int cnt = 0;
-        HashSet<Character> vowel = new HashSet<Character>();
-        vowel.add('a');
-        vowel.add('e');
-        vowel.add('i');
-        vowel.add('o');
-        vowel.add('u');
-        vowel.add('A');
-        vowel.add('E');
-        vowel.add('I');
-        vowel.add('O');
-        vowel.add('U');
+        if(s == null || s.length()==0) return s;
+        String vowels = "aeiouAEIOU";
+        char[] chars = s.toCharArray();
+        int start = 0;
+        int end = s.length()-1;
+        while(start<end){
         
-        for(int i = 0; i < s.length(); i++){
-            if(vowel.contains(s.charAt(i))){
-                pos[cnt] = i;
-                cnt++;
+            while(start<end && !vowels.contains(chars[start]+"")){
+                start++;
             }
-        }
         
-        char[] arr = new char[s.length()];
-        arr = s.toCharArray();
-        for(int i = 0; i < cnt; i++){
-            arr[pos[i]] = s.charAt(pos[cnt-i-1]);
+            while(start<end && !vowels.contains(chars[end]+"")){
+                end--;
+            }
+        
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+        
+            start++;
+            end--;
         }
-        return String.valueOf(arr);
+        return new String(chars);
     }
 }
