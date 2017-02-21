1d625fe365174eb0a2915b2946e6f992b54fd24b
Yagnesh Vadalia <yagnesh92@gmail.com>
2017-02-20 23:42:50 -0500

    Update binary tree maximum sum path.java

--- binary tree maximum sum path.java
+++ binary tree maximum sum path.java
@@ -1,1 +1,18 @@
+public int maxPathSum(TreeNode root) {
+  int[] max = new int[1];
+  max[0] = Integer.MIN_VALUE;
+  computeSum(root,max);
+  return max[0];
+}
 
+public int computeSum(TreeNode root, int[] max){
+  if(root == null) return 0;
+  
+  int left = computeSum(root.left, max);
+  int right = computeSum(root.right, max);
+  
+  int current = Math.max(max[0],Math.max(root.value + left, root.value+right));
+  max[0] = Math.max(max[0], Math.max(current, root.value+left+right));
+  
+  return current;
+}
